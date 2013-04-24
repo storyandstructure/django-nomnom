4f38ffa478d8504a13cd6e99ad0b2eb179591e89
kcharvey <kcharvey@gmail.com>
2013-04-24 12:01:59 -0400

    forget nomnom object tools  template. moveing func tests to webtest

--- nomnom/tests/functional_tests.py
+++ nomnom/tests/functional_tests.py
@@ -1,41 +1,24 @@
-from django.test import LiveServerTestCase
-from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from django_webtest import WebTest
+from bs4 import BeautifulSoup
 
-import time
+class NomnomTest(WebTest):
 
-class NomnomTest(LiveServerTestCase):
-
-    def setUp(self):
-		self.browser = webdriver.Firefox()
-		self.browser.implicitly_wait(3)
-
-    def tearDown(self):
-        self.browser.quit()
-
-    # As an administrator, I can go to /admin/ so that I can login.
-    def test_can_go_to_admin_url(self):
-		self.browser.get(self.live_server_url + '/admin/')
-		self.assertIn('Log in', self.browser.title)
-
-    # As an administrator, I can login so that I enter the admin site.
-    def test_can_login_admin_site(self):
-        self.fail('TODO')
-
-    # As an administrator, I can access the NomNom interface so that I can
-    # import or export data
-    #assert 'Django' in browser.title
-    #browser.get('http://localhost:8888')
     def test_can_access_nomnom(self):
-        self.fail('TODO')
+		# An administrator visits the admin site
+		response = self.app.get('/admin/')
+		soup = BeautifulSoup('<html>%s' % response.html)
+		title = soup.find('title')
+		self.assertEqual('Log in | Django site admin', title.text)
+		
+		self.fail('TODO')
 
-class NomnomRemoteTest(NomnomTest):
-	
-	live_server_url = 'http://localhost:8888'
-	
-	def setUp(self):
-		caps = webdriver.DesiredCapabilities.FIREFOX
-		self.browser = webdriver.Remote(
-			command_executor='http://192.168.0.187:4444/wd/hub',
-		    desired_capabilities=caps)
-		self.browser.implicitly_wait(3)
+# class NomnomRemoteTest(NomnomTest):
+# 	
+# 	live_server_url = 'http://localhost:8888'
+# 	
+# 	def setUp(self):
+# 		caps = webdriver.DesiredCapabilities.FIREFOX
+# 		self.browser = webdriver.Remote(
+# 			command_executor='http://192.168.0.187:4444/wd/hub',
+# 		    desired_capabilities=caps)
+# 		self.browser.implicitly_wait(3)
