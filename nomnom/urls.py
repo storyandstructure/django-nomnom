fe31ebf48802c7ce76018b226b3658be893e185a
Keith Donaldson <keith.donaldson@kdonaldsonllc.com>
2013-04-26 11:21:56 -0400

    Passing Kwargs to Context

--- nomnom/urls.py
+++ nomnom/urls.py
@@ -6,12 +6,8 @@ urlpatterns = patterns('',
     # used for the admin change list so that we can
     # overwrite the object tools ONLY
     #
-    # URL Patterns Needed
-    # /admin/APP_LABEL/MODEL_LABEL/import/, to import data
-    # from uploaded files to model;
-    # /admin/APP_LABEL/MODEL_LABEL/export/, to export
-    # data from a model;
-    #url(r'^admin/(?P<app_label>\w+)/(?P<model_name>\w+)/import/$', ImportPageView.as_view(app=app_label, mod=model_name,), name='import_data'),
-    url(r'^import/', ImportPageView.as_view(), name='import_data'),
-    url(r'^export/', ExportPageView.as_view(), name='export_data'),
+    # APP_LABEL
+    # MODEL_LABEL
+    url(r'^(?P<app_label>\w+)/(?P<model_name>\w+)/import/$', ImportPageView.as_view(), name='import_data'),
+    url(r'^(?P<app_label>\w+)/(?P<model_name>\w+)/export/$', ExportPageView.as_view(), name='export_data'),
     )
--- nomnom/views.py
+++ nomnom/views.py
@@ -1,8 +1,30 @@
 from django.views.generic.base import TemplateView
+from django.template.response import TemplateResponse
 
 
 class ImportPageView(TemplateView):
     template_name = "nomnom/import_data_form.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(ImportPageView, self).get_context_data(**kwargs)
+        context['app'] = self.kwargs.get("app_label")
+        context['model'] = self.kwargs.get("model_name")
+        return context
+    '''
+    def get(self, request, *args, **kwargs):
+        return TemplateResponse(request, self.get_template_name(),
+                                self.get_context_data())
+
+    def get_template_name(self):
+        """Returns the name of the template we should render"""
+        return "nomnom/import_data_form.html"
+
+    def get_context_data(self):
+        """Returns the data passed to the template"""
+        return {
+            "import": self.objects.all(),
+        }
+        '''
 
 class ExportPageView(TemplateView):
     template_name = "nomnom/export_data_form.html"
