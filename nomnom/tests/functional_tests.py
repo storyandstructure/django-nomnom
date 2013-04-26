c61465304ced3e0db7633dcbd52c173fb394f829
kcharvey <kcharvey@gmail.com>
2013-04-26 09:46:08 -0400

    users fixture, added to functional_test

--- nomnom/tests/functional_tests.py
+++ nomnom/tests/functional_tests.py
@@ -1,11 +1,15 @@
 from django_webtest import WebTest
 from bs4 import BeautifulSoup
 
+from django.contrib.admin.models import User
+
 
 class NomnomTest(WebTest):
+	
+    fixtures = ['users.json',]
 
     def test_can_access_nomnom(self):
-    # An administrator visits the admin site
+    	# An administrator visits the admin site
         response = self.app.get('/admin/')
         soup = BeautifulSoup('<html>%s' % response.html)
         title = soup.find('title')
@@ -13,12 +17,12 @@ class NomnomTest(WebTest):
 
         # As an administrator, I can click the Import button so that I can
         # import files.
-        user = self.app.get('/admin/auth/user')
+        user = self.app.get('/admin/auth/user/', user="admin")
         assert 'Import Users' in user.click('Import users')
 
         # As an administrator, I can click the Export button so that I can
         # export files.
-        user = self.app.get('/admin/auth/user')
-        assert 'Export Users' in user.click('Export users')
+        # user = self.app.get('/admin/auth/user')
+        # assert 'Export Users' in user.click('Export users')
 
         self.fail('TODO')
