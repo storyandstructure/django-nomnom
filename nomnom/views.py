d515f041b85211816c4859df267c4ef86aff1f41
Keith Donaldson <keith.donaldson@kdonaldsonllc.com>
2013-04-26 14:01:33 -0400

    Passing Model and App Names to Breadcrumbs

--- nomnom/views.py
+++ nomnom/views.py
@@ -1,5 +1,6 @@
 from django.views.generic.base import TemplateView
 from django.template.response import TemplateResponse
+#from nomnom.forms import ImportFileForm
 
 
 class ImportPageView(TemplateView):
@@ -10,21 +11,6 @@ class ImportPageView(TemplateView):
         context['app'] = self.kwargs.get("app_label")
         context['model'] = self.kwargs.get("model_name")
         return context
-    '''
-    def get(self, request, *args, **kwargs):
-        return TemplateResponse(request, self.get_template_name(),
-                                self.get_context_data())
-
-    def get_template_name(self):
-        """Returns the name of the template we should render"""
-        return "nomnom/import_data_form.html"
-
-    def get_context_data(self):
-        """Returns the data passed to the template"""
-        return {
-            "import": self.objects.all(),
-        }
-        '''
 
 class ExportPageView(TemplateView):
     template_name = "nomnom/export_data_form.html"
